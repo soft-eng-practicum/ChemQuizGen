7db2e8339fe8197d39ff1037db937d658aa7cd5f
mstiller7 <mstiller@ggc.edu>
2019-03-20 14:19:12 -0400

    push MolarityRev.ts to master.

---
+++ MolarityRev.ts
@@ -0,0 +1,17 @@
+
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"
+ "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html>
+  <head>
+    <title>503 Maximum threads for service reached</title>
+  </head>
+  <body>
+    <h1>Error 503 Maximum threads for service reached</h1>
+    <p>Maximum threads for service reached</p>
+    <h3>Guru Mediation:</h3>
+    <p>Details: cache-atl6242-ATL 1553105868 526817195</p>
+    <hr>
+    <p>Varnish cache server</p>
+  </body>
+</html>
